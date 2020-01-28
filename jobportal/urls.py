5845303892bdf181221e9d2beac71c4cd1d83024
rawbiin <58929863+rawbiin@users.noreply.github.com>
2020-01-28 15:26:13 +0545

    "User auth"

--- jobportal/urls.py
+++ jobportal/urls.py
@@ -21,7 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('jobs/',include('jobs.urls'))
+    path('jobs/',include('jobs.urls')),
+    path('user/',include('user.urls'))
     ]
 
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
