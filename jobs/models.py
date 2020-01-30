1a1799f96ad2a272fc4a793f11fef2a3ebcd6183
rawbiin <58929863+rawbiin@users.noreply.github.com>
2020-01-30 15:25:11 +0545

    "Api Complete"

--- jobs/models.py
+++ jobs/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Jobs(models.Model):
-    job_image=models.ImageField()
+    job_image=models.ImageField(null=True)
     jobs_name=models.TextField()
     jobs_description=models.TextField()
     jobs_Title=models.TextField()
